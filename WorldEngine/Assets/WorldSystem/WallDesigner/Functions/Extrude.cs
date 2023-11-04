8119db9f1f49488dda26246fe024a411baa8fc58
alibonyadi <ali.bonyadi.nezhad@gmail.com>
2023-11-04 15:38:00 +0330

    extrude save and load

--- WorldEngine/Assets/WorldSystem/WallDesigner/Functions/Extrude.cs
+++ WorldEngine/Assets/WorldSystem/WallDesigner/Functions/Extrude.cs
@@ -50,6 +50,70 @@ public class Extrude : FunctionItem, IFunctionItem
         attrebutes.Add(fl2);
     }
 
+    public override void LoadNodeConnections(SerializedFunctionItem item, List<FunctionItem> functionItems)
+    {
+        if (item.getnodeConnectedFI.Count > 0)
+            GetNodes[0].ConnectedNode = functionItems[item.getnodeConnectedFI[0]].GiveNodes[item.getnodeItems[0]];
+
+        if (item.givenodeConnectedFI.Count > 0)
+            GiveNodes[0].ConnectedNode = functionItems[item.givenodeConnectedFI[0]].GetNodes[item.givenodeItems[0]];
+
+        if (item.givenodeConnectedFI.Count > 1)
+            GiveNodes[1].ConnectedNode = functionItems[item.givenodeConnectedFI[1]].GetNodes[item.givenodeItems[1]];
+    }
+
+    public override void LoadSerializedAttributes(SerializedFunctionItem item)
+    {
+        Name = item.name;
+        ClassName = item.ClassName;
+
+        FloatAttrebute ta1 = (FloatAttrebute)attrebutes[0];
+        ta1.mFloat = float.Parse(item.attributeValue[0]);
+        attrebutes[0] = ta1;
+
+        FloatAttrebute att = (FloatAttrebute)attrebutes[1];
+        att.mFloat = float.Parse(item.attributeValue[1]);
+        attrebutes[1] = att;
+    }
+
+    public override SerializedFunctionItem SaveSerialize()
+    {
+        SerializedFunctionItem item = new SerializedFunctionItem();
+        item.name = Name;
+        item.ClassName = ClassName;
+        item.Position = position;
+        item.attributeName.Add("FloatAttrebute");
+
+        FloatAttrebute att1 = (FloatAttrebute)attrebutes[0];
+        string stringtexturePath = att1.mFloat.ToString();
+        item.attributeValue.Add(stringtexturePath);
+
+        FloatAttrebute att2 = (FloatAttrebute)attrebutes[1];
+        string stringtexturePath2 = att2.mFloat.ToString();
+        item.attributeValue.Add(stringtexturePath2);
+
+        if (GetNodes[0].ConnectedNode != null)
+        {
+            int connectedGetNodeNumber = WallEditorController.Instance.GetAllCreatedItems().IndexOf(GetNodes[0].ConnectedNode.AttachedFunctionItem);
+            item.getnodeConnectedFI.Add(connectedGetNodeNumber);
+            item.getnodeItems.Add(GetNodes[0].ConnectedNode.id);
+        }
+
+        if (GiveNodes[0].ConnectedNode != null)
+        {
+            int connectedGiveNodeNumber = WallEditorController.Instance.GetAllCreatedItems().IndexOf(GiveNodes[0].ConnectedNode.AttachedFunctionItem);
+            item.givenodeConnectedFI.Add(connectedGiveNodeNumber);
+            item.givenodeItems.Add(GiveNodes[0].ConnectedNode.id);
+        }
+
+        if (GiveNodes[1].ConnectedNode != null)
+        {
+            int connectedGiveNodeNumber = WallEditorController.Instance.GetAllCreatedItems().IndexOf(GiveNodes[1].ConnectedNode.AttachedFunctionItem);
+            item.givenodeConnectedFI.Add(connectedGiveNodeNumber);
+            item.givenodeItems.Add(GiveNodes[1].ConnectedNode.id);
+        }
+        return item;
+    }
 
     public object Execute(object mMesh, object id)
     {
@@ -173,11 +237,15 @@ public class Extrude : FunctionItem, IFunctionItem
         combinedMesh.CombineMeshes(combineInstances);
         //sideMesh.RecalculateBounds();
         //sideMesh.RecalculateNormals();
-        if(id == 0)
+        if((int)id == 0)
         {
-
+            output.mesh = extrudedMesh;
         }
-        output.mesh = combinedMesh;
+        else
+        {
+            output.mesh = sideMesh;
+        }
+        
         output.material = item.material;
         return output;
     }
