fa8ac991c8c8c35b11b91f1ab319b7b593287da1
alibonyadi <ali.bonyadi.nezhad@gmail.com>
2023-11-10 02:59:52 +0330

    completing execute and node names
    fixing nodes to Get and GivepropertyNode

--- WorldEngine/Assets/WorldSystem/WallDesigner/Scripts/Property.cs
+++ WorldEngine/Assets/WorldSystem/WallDesigner/Scripts/Property.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 namespace WallDesigner
 {
@@ -8,13 +9,15 @@ namespace WallDesigner
     {
         public Attrebute attrebute;
 
-        public List<Node> GetNodes;
-        public List<Node> GiveNodes;
-        protected Rect rect = new Rect();
+        public List<GetPropertyNode> GetNodes;
+        public List<GivePropertyNode> GiveNodes;
+        public Rect rect = new Rect();
 
         public Property(Rect r) 
         {
             rect = r;
+            GetNodes = new List<GetPropertyNode>();
+            GiveNodes = new List<GivePropertyNode>();
         }
 
         public virtual void Draw(Vector2 position)
@@ -39,6 +42,7 @@ namespace WallDesigner
         {
             GiveNodes[index].position = new Vector3(rect.x + rect.width + 5, rect.y + 5 + (15 * index) + 5, 0);
             DrawNodeLine(GiveNodes[index], false);
+            //Debug.Log("dddddd");
             GUI.color = GiveNodes[index].color;
             if (GUI.Button(new Rect(rect.x + rect.width, rect.y + 5 + (15 * index), 10, 10), ""))
             {
@@ -72,7 +76,14 @@ namespace WallDesigner
 
         public Attrebute Execute()
         {
-            return attrebute;
+            Attrebute item = attrebute;
+            if (GetNodes[0].ConnectedNode != null)
+            {
+                //Debug.Log(GetNodes[0].ConnectedNode.at)
+                GivePropertyNode givePropertyNode = (GivePropertyNode)GetNodes[0].ConnectedNode;
+                item = (Attrebute)givePropertyNode.AttachedProperty.Execute();
+            }
+            return item;
         }
     }
 }
