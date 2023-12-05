20af88e3c02ada2b0bdfc86d7dce5947718fe96b
alibonyadi <ali.bonyadi.nezhad@gmail.com>
2023-12-05 03:32:13 +0330

    adding class for saving building Directions
    Directions Like Street side or Alley side

---
+++ WorldEngine/Assets/WorldSystem/WallDesigner/Scripts/BuildingDirection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WallDesigner
+{
+    public class BuildingDirection
+    {
+        bool hasStreet = false;
+        bool hasAlley=false;
+
+        public List<Vector3> streetDirections = new List<Vector3>();
+        public List<Vector3> alleyDirections = new List<Vector3>();
+
+        public bool CheckStreet()
+        {
+            return hasStreet;
+        }
+
+        public bool CheckAlley()
+        {
+            return hasAlley;
+        }
+    }
+}
